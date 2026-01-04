70fce8f1ff4059b628d405d4045192189335fb97
Engineer187 <its.krishmistry@gmail.com>
2026-01-04 18:06:04 +0000

    Add files via upload

---
+++ face recongnision1.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import os
+harfile=r"opencv project\haarcascade_frontalface_default.xml"
+datasets=r"opencv project\data sets"
+sub_data="krish"
+path=os.path.join(datasets,sub_data)
+if not os.path.isdir(path):
+    os.mkdir(path)
+face_cascade=cv2.CascadeClassifier(harfile)
+webcam=cv2.VideoCapture(0)
+count=1
+while count<30:
+    value,image=webcam.read()
+    grey=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(grey,1.3,4)
+    for (x,y,w,h) in faces :
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,0),5)
+        face=grey[x:x+w,y:y+h]
+        face_resize=cv2.resize(face,(130,100))
+        cv2.imwrite("%s/%s.png"%(path,count),face_resize)
+    cv2.imshow("image",image)
+    a=cv2.waitKey(10)
+    if a == 27:
+        break
+    count=count+1
+    print("image_capture")
